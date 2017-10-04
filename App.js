312d59ecd90c09c60bbd82a51725823818331109
armon14 <armon14@hotmail.com>
2017-10-03 22:05:47 -0600

    API
    Updated to use API and added createcontact.js

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+  state = {
+    screen: 'list', // list, create
+    contacts: []
+  }
+  // 	contacts: [
+	//   {
+	//     "id": "ryan",
+	//     "name": "Ryan Florence",
+	//     "email": "ryan@reacttraining.com",
+	//     "avatarURL": "http://localhost:5001/ryan.jpg"
+	//   },
+	//   {
+	//     "id": "michael",
+	//     "name": "Michael Jackson",
+	//     "email": "michael@reacttraining.com",
+	//     "avatarURL": "http://localhost:5001/michael.jpg"
+	//   },
+	//   {
+	//     "id": "tyler",
+	//     "name": "Tyler McGinnis",
+	//     "email": "tyler@reacttraining.com",
+	//     "avatarURL": "http://localhost:5001/tyler.jpg"
+	//   }
+	// ]
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+  removeContact = (contact) => {
+  	this.setState((state) => ({
+  	  contacts: state.contacts.filter((c) => c.id !== contact.id)
+  	}))
+  }
+  render() {
+    return (
+      <div className="app">
+        {this.state.screen === 'list' && (
+          <ListContacts
+          	onDeleteContact={this.removeContact}
+          	contacts={this.state.contacts}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
+
+      </div>
+    )
+  }
+}
+
+export default App;
